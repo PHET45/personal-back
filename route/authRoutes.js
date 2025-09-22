b4703836ab9b6c7b69781e929f0dcd8773f14c8a
PHET45 <phnjk2000@gmail.com>
2025-09-23 03:53:37 +0700

    feat: middleware for login

--- route/authRoutes.js
+++ route/authRoutes.js
@@ -4,7 +4,7 @@ import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login",authenticate ,login);
 router.post("/register", register);
 router.get("/profile", authenticate, profile);
 
