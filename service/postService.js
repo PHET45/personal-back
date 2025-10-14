be2763f68b1b5bbf0f9ffb9f95845cdfdde37391
PHET45 <phnjk2000@gmail.com>
2025-10-15 03:34:57 +0700

    fix: update getAllPosts to include publishedOnly parameter for consistency

--- service/postService.js
+++ service/postService.js
@@ -6,7 +6,7 @@ export const PostService = {
   getAllPublishedPosts: (page, limit) => PostRepository.getAll({ publishedOnly: true, page, limit }),
   getPublishedPost: (id) => PostRepository.getById(id, { publishedOnly: true }),
 
-  getAllPosts: () => PostRepository.getAllSimple(),
+  getAllPosts: () => PostRepository.getAllSimple({ publishedOnly: false }),
   getPost: (id) => PostRepository.getById(id),
   createPost: (post) => PostRepository.create(post),
   updatePost: (id, post) => PostRepository.update(id, post),
